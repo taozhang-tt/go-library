ab9c9860ac569e33cf22719a41bc82d2ef91be28
TT <tt@TTs-MacBook-Pro.local>
2020-12-17 16:02:17 +0800

    leetcode 121 122

---
+++ leetcode/0121-best-time-to-buy-and-sell-stock/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+121. 买卖股票的最佳时机
+	https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
+题目描述
+	给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+	如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
+	注意：你不能在买入股票前卖出股票。
+示例 1:
+	输入: [7,1,5,3,6,4]
+	输出: 5
+	解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+		注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+示例 2:
+	输入: [7,6,4,3,1]
+	输出: 0
+	解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+*/
+func main() {
+	prices := []int{7,1,5,3,6,4}
+	fmt.Println(maxProfit(prices))
+}
+
+/*
+一次遍历
+*/
+func maxProfit(prices []int) int {
+	if len(prices) < 2 {
+		return 0
+	}
+	ans, min := 0, prices[0]
+	for i:=1; i<len(prices); i++ {
+		if ans < prices[i]-min {
+			ans = prices[i]-min
+		}
+		if prices[i] < min {
+			min = prices[i]
+		}
+	}
+	return ans
+}
+
+func max(nums ...int) int {
+	ans := nums[0]
+	for i:=1; i<len(nums); i++ {
+		if nums[i] < ans {
+			ans = nums[i]
+		}
+	}
+	return ans
+}
